d56be01af70eacf5b2585b07511936c7f66ac356
Philippe Gerum <rpm@xenomai.org>
2022-06-05 19:29:23 +0200

    clock: introduce the built-in wall clock
    At this chance, prefer 'steady' to 'monotonic' in naming.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- src/clock.rs
+++ src/clock.rs
@@ -19,9 +19,9 @@ use evl_sys::{
     BuiltinClock
 };
 
-#[derive(Debug)]
-pub struct MonoClock;
-impl Clock for MonoClock {
+pub struct CoreClock(isize);
+
+impl Clock for CoreClock {
     type T = u64;
     const SCALING_FACTOR: Fraction = Fraction::new(1, 1_000_000_000); // ns
 
@@ -30,14 +30,14 @@ impl Clock for MonoClock {
             tv_sec: 0,
             tv_nsec: 0,
         };
-        unsafe { evl_read_clock(BuiltinClock::MONOTONIC as c_int, &mut now) };
+        unsafe { evl_read_clock(self.0 as c_int, &mut now) };
         let now_ns: u64 = now.tv_sec as u64 * 1_000_000_000 + now.tv_nsec as u64;
         Ok(Instant::new(now_ns))
     }
 }
 
-impl MonoClock {
-    pub fn sleep_until(&self, timeout: Instant<MonoClock>) -> Result<(), io::Error> {
+impl CoreClock {
+    pub fn sleep_until(&self, timeout: Instant<CoreClock>) -> Result<(), io::Error> {
         let dur = timeout.duration_since_epoch();
         let secs: Seconds<u64> = Seconds::try_from(dur).unwrap();
         let nsecs: Nanoseconds<u64> = Nanoseconds::<u64>::try_from(dur).unwrap() % secs;
@@ -45,7 +45,7 @@ impl MonoClock {
             tv_sec: secs.integer() as time_t,
             tv_nsec: nsecs.integer() as c_long,
         };
-        let ret: c_int = unsafe { evl_sleep_until(BuiltinClock::MONOTONIC as c_int, &date) };
+        let ret: c_int = unsafe { evl_sleep_until(self.0 as c_int, &date) };
         match ret {
             0 => return Ok(()),
             _ => return Err(io::Error::from_raw_os_error(-ret)),
@@ -55,3 +55,8 @@ impl MonoClock {
         self.try_now().unwrap()
     }
 }
+
+/// EVL implements two builtin clocks: monotonic (aka POSIX
+/// CLOCK_MONOTONIC) and adjustable wallclock (aka POSIX CLOCK_REALTIME).
+pub const STEADY_CLOCK: CoreClock = CoreClock(BuiltinClock::MONOTONIC as isize);
+pub const WALL_CLOCK: CoreClock = CoreClock(BuiltinClock::REALTIME as isize);
