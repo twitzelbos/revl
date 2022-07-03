dcf8999780049cc280cd5ce3126d9a44d56cf902
Philippe Gerum <rpm@xenomai.org>
2022-07-03 16:17:44 +0200

    thread: fixup the documentation
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- src/thread.rs
+++ src/thread.rs
@@ -1,7 +1,9 @@
 //! Real-time thread.
 //!
 //! EVL threads are native threads originally, which are extended with
-//! real-time capabilities once attached to the EVL core.
+//! real-time capabilities once attached to the EVL core. See [this
+//! document](https://evlproject.org/core/user-api/thread/) for an
+//! introduction to EVL threads.
 
 use std::thread;
 use std::ptr;
@@ -43,8 +45,6 @@ impl Builder {
     /// - `unicast`: if observable, specifies whether notifications
     /// should be sent to a single observer instead of broadcast
     /// to all of them.
-    ///
-    ///
     pub fn new() -> Self {
         Self {
             name: None,
@@ -53,22 +53,53 @@ impl Builder {
             unicast: false,
         }
     }
+    /// Set the thread name. This name must conform to the [naming
+    /// convention](https://evlproject.org/core/user-api/#element-naming-convention)
+    /// for EVL elements.
     pub fn name(mut self, name: &str) -> Self {
         self.name = Some(name.to_string());
         self
     }
+    /// Set the thread visibility to 'public', as defined by this
+    /// [document](https://evlproject.org/core/user-api/#evl-fs-hierarchy).
+    ///
+    /// ```no_run
+    /// use revl::thread;
+    /// use std::path;
+    ///
+    /// let thread = thread::Builder::new().name("foo_thread").public().attach().unwrap();
+    /// let path = Path::new("/dev/evl/threads/foo_thread");
+    /// assert_eq!(path.exists(), true);
+    /// ```
     pub fn public(mut self) -> Self {
         self.visible = true;
         self
     }
+    /// Set the thread visibility to 'private', as defined by this
+    /// [document](https://evlproject.org/core/user-api/#evl-fs-hierarchy). This
+    /// is the default setting.
+    ///
+    /// ```no_run
+    /// use revl::thread;
+    /// use std::path;
+    ///
+    /// let thread = thread::Builder::new().name("bar_thread").private().attach().unwrap();
+    /// let path = Path::new("/dev/evl/threads/bar_thread");
+    /// assert_eq!(path.exists(), false);
+    /// ```
     pub fn private(mut self) -> Self {
         self.visible = false;
         self
     }
+    /// Make the thread observable for health monitoring purpose, as
+    /// defined by this
+    /// [document](https://evlproject.org/core/user-api/observable/#observable-thread).
     pub fn observable(mut self) -> Self {
         self.observable = true;
         self
     }
+    /// Enable unicast mode for an observable thread, restricting the
+    /// notification of events to a single observer.
     pub fn unicast(mut self) -> Self {
         self.unicast = true;
         self
@@ -81,16 +112,12 @@ impl Builder {
     /// ```no_run
     /// use revl::thread;
     ///
-    /// fn attach_current_thread_using_builder() -> Result<Thread, std::io::Error> {
-    ///     let me = thread::Builder::new()
-    ///		.name("myself")
-    ///		.private().
-    ///		observable()
-    ///		.attach()?;
-    ///     Ok(me)
-    /// }
+    /// thread::Builder::new()
+    ///		.name("foo_thread")
+    ///		.private()
+    ///		.observable()
+    ///		.attach().expect("cannot attach thread to EVL core");
     /// ```
-    ///
     pub fn attach(self) -> Result<Thread, Error> {
         Thread::attach(self)
     }
@@ -121,14 +148,18 @@ impl Builder {
     ///
     /// ## Join errors
     ///
-    /// [`AlreadyExists`][`std::io::ErrorKind`] is returned if an
+    /// On error attaching the new thread to the core,
+    /// [`join()`][`thread::JoinHandle::join`] may return any of the
+    /// following statuses:
+    ///
+    /// * [`AlreadyExists`][`std::io::ErrorKind`] is returned if an
     /// existing thread already goes by the same name.
     ///
-    /// [`InvalidInput`][`std::io::ErrorKind`] may denote a badly formed
+    /// * [`InvalidInput`][`std::io::ErrorKind`] may denote a badly formed
     /// name. Check these
     /// [rules](https://evlproject.org/core/user-api/#element-naming-convention).
     ///
-    /// [`PermissionDenied`][`std::io::ErrorKind`] means that the
+    /// * [`PermissionDenied`][`std::io::ErrorKind`] means that the
     /// calling thread is not allowed to lock memory by a call to
     /// [mlockall(2)](https://man7.org/linux/man-pages/man2/mlock.2.html),
     /// which is a showstopper for real-time execution.
@@ -168,50 +199,40 @@ unsafe impl Sync for Thread {}
 impl Thread {
     /// Attach the calling thread to the EVL core.
     ///
-    /// This function is an alternative way to calling the attach()
-    /// method from the thread::Builder factory in order to attach the
-    /// caller to the EVL core.
-    ///
-    /// # Arguments
-    ///
-    /// * `builder`: a builder struct containing the EVL-specific
-    /// properties of the thread.
+    /// The [`Builder`] struct contains the EVL-specific properties to
+    /// use.
     ///
     /// # Errors
     ///
-    /// * Error(AlreadyExists) means the thread name is conflicting with
-    /// an existing thread name.
+    /// * [`AlreadyExists`][`std::io::ErrorKind`] means the thread
+    /// name is conflicting with an existing thread name.
     ///
-    /// * Error(InvalidInput) means that the thread name contains
-    /// invalid characters: such name must contain only valid
-    /// characters in the context of a Linux file name.
+    /// * [`InvalidInput`][`std::io::ErrorKind`] means that the thread
+    /// name contains invalid characters: such name must contain only
+    /// valid characters in the context of a Linux file name.
     ///
-    /// * Error(Other) may mean that either the EVL core is not
-    /// enabled in the kernel, or there is an ABI mismatch between the
-    /// underlying [evl-sys
+    /// * [`Other`][`std::io::ErrorKind`] may mean that either the EVL
+    /// core is not enabled in the kernel, or there is an ABI mismatch
+    /// between the underlying [evl-sys
     /// crate](https://source.denx.de/Xenomai/xenomai4/evl-sys) and
     /// the EVL core. See [these
     /// explanations](https://evlproject.org/core/under-the-hood/abi/)
     /// for the latter.
     ///
-    /// * Error(PermissionDenied) means that the calling context is
-    /// not granted the privileges required by the attachment
-    /// operation, such as locking memory via the
+    /// * [`PermissionDenied`][`std::io::ErrorKind`] means that the
+    /// calling context is not granted the privileges required by the
+    /// attachment operation, such as locking memory via the
     /// [mlockall(2)](http://man7.org/linux/man-pages/man2/mlock.2.html)
     /// system call.
     ///
     /// # Examples
     ///
-    /// ```rust
-    /// use revl::thread::{Builder, Thread};
+    /// ```no_run
+    /// use revl::thread;
     ///
-    /// fn attach_current_thread() -> Result<Thread, std::io::Error> {
-    ///     let props = Builder::new().name("myself").public();
-    ///     let me = Thread::attach(props)?;
-    ///     Ok(me)
-    /// }
+    /// let props = thread::Builder::new().name("foo_thread").public();
+    /// thread::Thread::attach(props).expect("cannot attach thread to EVL core");
     /// ```
-    ///
     pub fn attach(builder: Builder) -> Result<Self, Error> {
 	let mut c_flags = CloneFlags::PRIVATE.bits() as c_int;
         if builder.visible {
@@ -247,16 +268,15 @@ impl Thread {
     /// target thread wakes up with an interrupted call status on
     /// return.
     ///
-    /// # Example
+    /// # Examples
     ///
-    /// ```rust
-    /// use revl::thread::Thread;
+    /// ```no_run
+    /// use revl::thread;
     ///
-    /// fn unblock_some_thread(t: &Thread) -> Result<(), std::io::Error> {
+    /// fn unblock_some_thread(t: &thread::Thread) -> Result<(), std::io::Error> {
     ///     t.unblock()
     /// }
     /// ```
-    ///
     pub fn unblock(&self) -> Result<(), Error> {
 	    let ret: c_int = unsafe { evl_unblock_thread(self.0) };
 	    match ret {
@@ -264,7 +284,6 @@ impl Thread {
                 _ => return Err(Error::from_raw_os_error(-ret)),
 	    }
     }
-
     /// Demote the target thread to in-band context.
     ///
     /// Demoting a thread means to force it out of any real-time
@@ -273,16 +292,15 @@ impl Thread {
     /// details and caveat
     /// [here](https://evlproject.org/core/user-api/thread/#evl_demote_thread).
     ///
-    /// # Example
+    /// # Examples
     ///
-    /// ```rust
-    /// use revl::thread::Thread;
+    /// ```no_run
+    /// use revl::thread;
     ///
-    /// fn demote_some_thread(t: &Thread) -> Result<(), std::io::Error> {
+    /// fn demote_some_thread(t: &thread::Thread) -> Result<(), std::io::Error> {
     ///     t.demote()
     /// }
     /// ```
-    ///
     pub fn demote(&self) -> Result<(), Error> {
 	    let ret: c_int = unsafe { evl_demote_thread(self.0) };
 	    match ret {
@@ -290,26 +308,17 @@ impl Thread {
                 _ => return Err(Error::from_raw_os_error(-ret)),
 	    }
     }
-
-    /// Set the scheduling attributes of a thread.
+    /// Set the scheduling attributes of a thread to `param`.
     ///
-    /// Changes the scheduling attributes for the thread it is called
-    /// on.
+    /// # Examples
     ///
-    /// # Argument
+    /// ```no_run
+    /// use revl::thread;
     ///
-    /// * `param` - The new scheduling parameters.
-    ///
-    /// # Example
-    ///
-    /// ```rust
-    /// use revl::thread::Thread;
-    ///
-    /// fn set_thread_sched(t: &Thread) -> Result<(), std::io::Error> {
+    /// fn set_thread_sched(t: &thread::Thread) -> Result<(), std::io::Error> {
     ///     t.set_sched(SchedFifo { prio: 42 })
     /// }
     /// ```
-    ///
     pub fn set_sched(&self, param: impl PolicyParam) -> Result<(), Error> {
 	let c_attrs_ptr: *const evl_sched_attrs = &param.to_attr().0;
 	let ret: c_int = unsafe { evl_set_schedattr(self.0, c_attrs_ptr) };
